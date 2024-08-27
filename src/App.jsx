e7c41e0704f6324f11f9c6a195591d7774795c7e
Aqdas <aqdas.rehmanbca-2022@asb.edu.in>
2024-08-27 14:39:01 +0530

    adding hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import "./Assets/css/font.css"
+import "./Assets/css/var.css"
 import Header from './componenets/Header';
 import Home from './componenets/Home';
 import Work from './componenets/Work';
@@ -8,7 +10,7 @@ import About from './componenets/About';
 
 function App() {
   return (
-    <Router basename='/Sehej-portfolio'>
+    <Router>
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
@@ -16,7 +18,6 @@ function App() {
         <Route path='/photography' element={<Photography/>}></Route>
         <Route path='/about'element={<About/>}></Route>
       </Routes>
-      
     </Router>
   );
 }
