061f8dd2d63530c7c6e0f9b728205d6eea9300c8
Aqdas <aqdas.rehmanbca-2022@asb.edu.in>
2024-09-02 13:47:38 +0530

    adding footer and about components

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import '../Assets/css/footer.css'
+import facebook_icon from '../Assets/icons/facebook.webp'
+import instagram_icon from '../Assets/icons/instagram.webp'
+import pintrest_icon from '../Assets/icons/pintrest.webp'
+import vimeo_icon from '../Assets/icons/vimeo.webp'
+import youtube_icon from '../Assets/icons/youtube.webp'
+import phone_icon from '../Assets/icons/phone.webp'
+import mail_icon from '../Assets/icons/mail.webp'
+import location_icon from '../Assets/icons/location.webp'
 
 const Footer = () => {
+
+  const social_data =[
+    {name: 'Facebook',          img:facebook_icon},
+    {name: 'Instagram',         img:instagram_icon},
+    {name: 'Pintrest',          img:pintrest_icon},
+    {name: 'Vimeo',             img:vimeo_icon},
+    {name: 'Youtube',           img:youtube_icon}
+  ]
+
   return (
     <>
+      <div className="footer-container">
+        <div className="main-footer">
+          <h1 className="footer-heading">Sehej</h1>
+          <ul className="social-link-list">
+            {
+              social_data.map((i)=>{
+                return(
+                  <li className="social_link"><img src={i.img} alt={i.name} className="social-link-img" /></li>
+                )
+              })
+            }
+           </ul>
+          <div className="footer-contact">
+            <div className="contact-item"><img src={phone_icon} alt="contact" className="contact-icon" /><p className="contact-para">9891234678</p></div>
+            <div className="contact-item"><img src={mail_icon} alt="contact" className="contact-icon" /><p className="contact-para">sehej@xyz.com</p></div>
+            <div className="contact-item"><img src={location_icon} alt="location" className="contact-icon" /><p className="contact-para">D-913, New friends colony,<br /> Delhi - 110092</p></div>
+          </div>
+        </div>
+        <div className="copyright-container">
+          <p className="copyright-para">&copy; 2024 All right resrved | Developed by Aqdas Rehman</p>
+        </div>
+      </div>
       
     </>
   )
